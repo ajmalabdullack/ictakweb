6db2856b9020ef2ddea248bcd354e3fdb41a4c30
ajmalabdullack <ajmalckaju@gmail.com>
2021-07-17 20:51:31 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcademicMembershipComponent } from './academic-membership/academic-membership.component';
 import { PartnershipComponent } from './partnership/partnership.component';
 
-const routes: Routes = [{path:"partnership",component:PartnershipComponent}];
+const routes: Routes = [{path:"",component:AcademicMembershipComponent},{path:"partnership",component:PartnershipComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PartnershipComponent } from './partnership/partnership.component';
+import { AcademicMembershipComponent } from './academic-membership/academic-membership.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PartnershipComponent
+    PartnershipComponent,
+    AcademicMembershipComponent
   ],
   imports: [
     BrowserModule,
